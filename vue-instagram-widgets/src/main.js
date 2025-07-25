9f36d12284439f8081c598d411376e9c48affc53
Rafael Santos <rafael.silva@gran.com>
2025-07-25 09:46:16 -0300

    injetando main.css

--- vue-instagram-widgets/src/main.js
+++ vue-instagram-widgets/src/main.js
@@ -1,8 +1,17 @@
 import { createApp } from 'vue';
-
-// Importação dos componentes Vue
 import ElfsightFeed from './components/ElfsightFeed.vue';
 import Servicos from './components/Servicos.vue';
+
+async function injectCSS(url) {
+    const res = await fetch(url);
+    const css = await res.text();
+    const style = document.createElement('style');
+    style.textContent = css;
+    document.head.appendChild(style);
+}
+
+// Chamada para injetar o CSS externo no head
+injectCSS('/static/main.css');
 
 // Função reutilizável que verifica se o elemento existe e monta o componente
 function mountIfExists(component, mountId) {
