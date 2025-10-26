44582ea96f3b6de706e677e746ad96926e31ed74
Rafael Santos <rafael.silva@gran.com>
2025-10-26 10:31:36 -0300

    fix: correcao caminho dos statics files

--- src/app/routes/static_files.py
+++ src/app/routes/static_files.py
@@ -4,9 +4,12 @@ from flask import Blueprint, send_from_directory, abort
 bp = Blueprint("static_files", __name__)
 ALLOWED_EXTENSIONS = {'.json', '.js', '.html', '.css', '.jpg', '.png'}
 
-def allowed_file(filename): return os.path.splitext(filename)[1] in ALLOWED_EXTENSIONS
+def allowed_file(filename):
+    return os.path.splitext(filename)[1] in ALLOWED_EXTENSIONS
 
 @bp.get("/static/instagram/<path:filename>")
 def serve_static_instagram(filename):
-    if not allowed_file(filename): abort(404)
-    return send_from_directory('static/instagram', filename)
+    if not allowed_file(filename):
+        abort(404)
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../static/instagram"))
+    return send_from_directory(base_dir, filename)
