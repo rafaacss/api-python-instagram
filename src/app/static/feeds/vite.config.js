d33a2608ad590be3fc48799f85e18ef1c61d2f33
Rafael Santos <rafael.silva@gran.com>
2025-10-31 20:14:42 -0300

    teste

--- src/app/static/feeds/vite.config.js
+++ src/app/static/feeds/vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig({
   plugins: [vue()],
   root: ".",
-  base: "/feeds/",          // <- IMPORTANTÍSSIMO
+  base: "/static/feeds/",          // <- IMPORTANTÍSSIMO
   build: {
     outDir: "dist",
     emptyOutDir: true,
